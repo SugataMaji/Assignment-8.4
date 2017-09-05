f8b4b1f0675cace566cbe4523fbee15f9f062440
SugataMaji <sugatamaji94@gmail.com>
2017-09-05 21:57:17 +0530

    Add files via upload

---
+++ src/app/services/cricketer.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ICricketList } from 'app/interface/cricketer-list';
+import { IPlayerType } from 'app/interface/player-type';
+
+@Injectable()
+export class CricketerService {
+
+  cricketersList: ICricketList[] = [];
+
+  /** Add cricketer in the array List. */
+  addCricketer(cricketerDetail: ICricketList) {
+    console.log('Retrieving data');
+    this.cricketersList.unshift(cricketerDetail);
+  }
+
+  /**Get the cricket list from the array. */
+  getCricket(): ICricketList[] {
+    return this.cricketersList;
+  }
+
+
+}
